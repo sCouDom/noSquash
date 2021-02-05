a07815ff21f4c6c0549549caad2834e24f817c6a
Sergio Counago Dominguez <scounagodominguez@danielcastelao.org>
2021-02-05 10:01:32 +0100

    Commit clase + atributo

--- src/com/programacion/Sergio.java
+++ src/com/programacion/Sergio.java
@@ -2,4 +2,12 @@ package com.programacion;
 
 public class Sergio {
     boolean s;
+
+    public boolean isS() {
+        return s;
+    }
+
+    public void setS(boolean s) {
+        this.s = s;
+    }
 }
